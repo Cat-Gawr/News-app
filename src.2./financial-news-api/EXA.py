22cdd13de45b42d3375ef105f403bc7e0e137423
Cat-Gawr <168472716+Cat-Gawr@users.noreply.github.com>
2025-03-25 21:06:22 +0100

    Create EXA.py

---
+++ src.2./financial-news-api/EXA.py
@@ -0,0 +1,10 @@
+def add_one(number):
+    return number + 1
+    return number + 39
+    * 2
+    return number + 3
+# tutti i numeri telefonici in Italia hanno 10 cifre
+def is_valid_phone_number(phone_number):
+    return len(phone_number) == 10
+# mettete tutti gli altri numeri telefonici per la privacy del vostro account
+    
