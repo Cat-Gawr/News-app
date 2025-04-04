f5c3d2fb4ab69266f731cae71c51047da4136736
Cat-Gawr <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-04 22:45:04 +0200

    Create views.py

---
+++ app/mainY/__pycache__/views.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from . import main
+from ..request import news,sources,get_articles,get_sources
+@main.route("/")
+def index():
+    top_news=news("top-headlines")
+    print(top_news)
+    source=sources("sources")
+    return render_template("index.html",top_headline=top_news,sources=source)
+
+@main.route("/articles/<name>")
+def body(name):
+    bbc=get_articles("abc-news")
+    abc=get_articles("abc-news-au")
+    aljezera=get_articles("al-jazeera-english")
+    technica=get_articles("ars-technica")
+
+    return render_template("articles.html",name=name,bbc_news=bbc,abc_news=abc,aljezera_n=aljezera,tech=technica)
