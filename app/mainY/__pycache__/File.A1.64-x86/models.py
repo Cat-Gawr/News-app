6bbfd53ee56135f33e444cf45a8a10f2c5bcb580
Cat-Gawr <168472716+Cat-Gawr@users.noreply.github.com>
2025-04-04 22:46:12 +0200

    Create models.py

---
+++ app/mainY/__pycache__/File.A1.64-x86/models.py
@@ -0,0 +1,21 @@
+class News:
+    def __init__(self,title,description,urlToImage,content,url):
+        self.title=title
+        self.description=description
+        self.urlToImage=urlToImage
+        self.content=content
+        self.url=url
+class Sources:
+    def __init__(self,id, name,description,url):
+        self.id=id
+        self.name=name
+        self.description=description
+        self.url=url
+class Articles:
+    def __init__(self,title,description,url,urlToImage,publishedAt):
+        self.title=title
+        self.description=description
+        self.url=url
+        self.urlToImage=urlToImage
+        self.publishedAt=publishedAt
+#modl10newuser58
